9d3070268cfa4994b74b7c800c27ba1d451432d8
SimonMellado <simon.mellado02@inacapmail.cl>
2026-04-24 11:28:02 -0400

    fix login structure and env production

--- web/src/pages/login.jsx
+++ web/src/pages/login.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 function Login() {
   const API = process.env.REACT_APP_API || "https://veido-saas.onrender.com";
 
   const login = () => {
     window.location.href = `${API}/auth/login`;
   };
-}
+
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>Nexora Dashboard</h1>
       <button onClick={login}>Login con Discord</button>
     </div>
   );
+}
 
 export default Login;
