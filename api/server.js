4934a4081143f8812729f829ad1a6a30530e2e22
SimonMellado <simon.mellado02@inacapmail.cl>
2026-05-05 22:11:19 -0400

    fix: pasar usuario por URL al redirigir desde OAuth

--- api/server.js
+++ api/server.js
@@ -16,6 +16,11 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
